61b5218a577021e71e65c7bd9079624e1b725125
SrAlch <daniel.contero@gmail.com>
2025-02-04 02:10:42 +0000

    Added RabbitMQ And Masstransit to the services, decoupled them so they can publish and consume messages from a queue and have a local copy of relevant parameters

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Contracts;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
 using Play.Common;
@@ -14,17 +16,19 @@ namespace Play.Catalog.Service.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IRepository<Item> itemsRepository;
+        private readonly IPublishEndpoint publishEndpoint;
 
-        public ItemsController(IRepository<Item> itemsRepository)
+        public ItemsController(IRepository<Item> itemsRepository, IPublishEndpoint publishEndpoint)
         {
             this.itemsRepository = itemsRepository;
+            this.publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<ItemDto>> GetAsync()
+        public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
             var items = (await itemsRepository.GetAllAsync()).Select(item => item.AsDto());
-            return items;
+            return Ok(items);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id)
@@ -50,6 +54,8 @@ namespace Play.Catalog.Service.Controllers
                 CreatedDate = DateTimeOffset.UtcNow
             };
             await itemsRepository.CreateAsync(item);
+
+            await publishEndpoint.Publish(new CatalogItemCreated(item.Id, item.Name, item.Description));
             return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
         }
 
@@ -69,6 +75,8 @@ namespace Play.Catalog.Service.Controllers
 
             await itemsRepository.UpdateAsync(exisitngItem);
 
+            await publishEndpoint.Publish(new CatalogItemUpdated(exisitngItem.Id, exisitngItem.Name, exisitngItem.Description));
+
             return NoContent();
         }
 
@@ -83,6 +91,8 @@ namespace Play.Catalog.Service.Controllers
             }
 
             await itemsRepository.RemoveAsync(item.Id);
+
+            await publishEndpoint.Publish(new CatalogItemDeleted(id));
 
             return NoContent();
         }
